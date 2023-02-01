4734bbdfc6f77be8d3810b8cee95cde5aebb4171
James Miller <jmiller24@students.polytechnic.org>
2023-02-01 09:24:02 -0800

    Wrote Passenger class

--- Passenger.java
+++ Passenger.java
@@ -1,7 +1,20 @@
 public class Passenger {
     //fields//
+    private int destination;
 
     //constructors//
+    public Passenger(int myDestination) {
+        destination = myDestination; 
+    }
 
     //methods//
+    public int getDest() {
+        return destination; 
+    }
+
+    public String toString() {
+        return super.toString() +
+        "Passenger" +
+        "[dest=" + destination; 
+    }
 }
