b180f628369dc079e9cda8813303190c0cd29787
James Miller <jmiller24@students.polytechnic.org>
2023-02-01 09:51:56 -0800

    Wrote primary methods for station

--- Station.java
+++ Station.java
@@ -2,11 +2,30 @@ import java.util.*;
 
 public class Station {
     //fields//
+    private ArrayList<Passenger> passengers;
+    private ArrayList<Passenger> successes; 
 
     //constructors//
+    public Station() {
+        passengers = new ArrayList<Passenger>(); 
+    }
 
     //methods//
-    public void recieve() {
-        ;
+    public void addPassenger(Passenger newPassenger) {
+        passengers.add(newPassenger);
     }
+
+    public void recieve(Passenger p) {
+        successes.add(p);
+    }
+
+    public Passenger load() {
+        return passengers.get(0);
+    }
+
+    public int getPassCount() {
+        return passengers.size();
+    }
+
+    
 }
