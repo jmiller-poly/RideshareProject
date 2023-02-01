7e9a50895d4182dd6165cde190e232bdfd3175bd
James Miller <jmiller24@students.polytechnic.org>
2023-02-01 09:29:10 -0800

    Started on Car and Station classes for working in tandem.

--- Car.java
+++ Car.java
@@ -1,7 +1,46 @@
+import java.util.*;
+
 public class Car {
     //fields//
+    private int currentStation;
+    private boolean goingForward;
+    private ArrayList<Passenger> passengers;
+    private int revenue;
 
     //constructors//
+    public Car(int firstStation, boolean startForward) {
+        currentStation = firstStation;
+        goingForward = startForward; 
+        passengers = new ArrayList<Passenger>();
+    }
+
+    public Car(int firstStation) {
+        currentStation = firstStation;
+        goingForward = true;
+        passengers = new ArrayList<Passenger>();
+    }
+
+    public Car() {
+        currentStation = 0;
+        goingForward = true;
+        passengers = new ArrayList<Passenger>();
+    }
 
     //methods//
+    
+    public void dropOff(Station station) {
+        for(int i = 0; i < 3; i++) {
+            Passenger currPass = passengers.get(i);
+            if(currPass.getDest() == currentStation) {
+                station.receive(passengers.get(i)); 
+                passengers.remove(i);
+            }
+        }
+    }
+
+    public void pickUp(Station station) {
+        for(int i = passengers.size(); i < 3; i++) {
+
+        }
+    }
 }
--- Station.java
+++ Station.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class Station {
     //fields//
 
     //constructors//
 
     //methods//
+    public void recieve() {
+        ;
+    }
 }
