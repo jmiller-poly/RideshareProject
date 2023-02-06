8cf9f4688329836ea05b775a346b91d7189ccbe8
Nicholas Gomez <ngomez24@students.polytechnic.org>
2023-02-06 13:22:43 -0800

    comments

--- Passenger.java
+++ Passenger.java
@@ -9,7 +9,7 @@ public class Passenger {
     private int destination;
 
     //constructors//
-    public Passenger(int myDestination) {
+    public Passenger(int myDestination) { 
         destination = myDestination; 
     }
 
@@ -18,7 +18,7 @@ public class Passenger {
         return destination;  //calls the destination variable
     }
 
-    public String toString() { 
+    public String toString() { //toString to fufil recquirement, returns value fo destination in a String format
         return super.toString() +
         "Passenger" +
         "[dest=" + destination; 
