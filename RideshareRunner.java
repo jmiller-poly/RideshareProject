012a1e31719daa81793a4de3b47f4bbb567c2e19
James Miller <jmiller24@students.polytechnic.org>
2023-02-09 08:43:30 -0800

    Improved tester

--- RideshareRunner.java
+++ RideshareRunner.java
@@ -6,11 +6,11 @@
 
 public class RideshareRunner {
     public static void main(String[] args) {
-        testRoad(31, 20, 50, false, false);
-        testRoad(31, 10, 30, false, false);
+        loopRoad(31, 20, 50, false, false);
+        loopRoad(31, 10, 30, false, false);
     }
 
-    public static void testRoad(int stations, int cars, int passengers, boolean testStatements, boolean lastCheck) {
+    public static int[] testRoad(int stations, int cars, int passengers, boolean testStatements, boolean lastCheck) {
         Road LakeSt = new Road();
         for(int s = 0; s < stations; s++) {
             LakeSt.addStation();
@@ -30,8 +30,24 @@ public class RideshareRunner {
         if(lastCheck) {
             System.out.println(LakeSt.toString());
         }
-        System.out.println("\nInformation for a road with " + stations + " stations, " + cars + " cars, and " + passengers + " passengers: ");
-        System.out.println("Total revenue per car: " + LakeSt.getTotalRevenue()/((double) cars));
-        System.out.println("Passengers who did not make it to their destination: " + LakeSt.getLeftOverPassengers());
+        int[] info = new int[2];
+        info[0] = LakeSt.getTotalRevenue();
+        info[1] = LakeSt.getLeftOverPassengers();
+
+        return info;
+    }
+
+    public static void loopRoad(int stations, int cars, int passengers, boolean testStatements, boolean lastCheck) {
+        double avgRev = 0.0;
+        double avgLOP = 0.0;
+        for(int i = 0; i < 50; i++) {
+            int[] info = testRoad(stations, cars, passengers, testStatements, lastCheck);
+            avgRev += info[0];
+            avgLOP += info[1];
+        }
+        System.out.println("\nAverage information for a road with " + stations + " stations, " + cars + " cars, and " + passengers + " passengers: ");
+        System.out.println("Average total revenue: " + avgRev/50);
+        System.out.println("Per car: " + avgRev/(50*cars));
+        System.out.println("Average passengers who did not make it to their destination: " + avgLOP/50);
     }
 }
